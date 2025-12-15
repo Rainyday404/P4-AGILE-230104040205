5229e32c5d411f2b9d01b1f7192434190a06eba8
AdminBocchi <ygyryukaa@gmail.com>
2025-12-15 21:19:09 +0800

    fix: remove temporary submit folder and zip artifacts

--- submit/services/notification-service/src/server.ts
+++
@@ -1,5 +0,0 @@
-import app from './index';
-import { logger } from '../../../utils';
-
-const port = Number(process.env.PORT) || 5003;
-app.listen(port, () => logger.info({ port }, 'notification-service listening'));
